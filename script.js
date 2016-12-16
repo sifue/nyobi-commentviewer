6197de7221d0a9abb9ac0cf4c7e2e237fb10e0b8
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-12-16 23:31:32 +0900

    運営コメントに対応

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+// create comment area
 var commentTextArea = document.querySelector('#comment-text-area');
 if (!commentTextArea) {
   var column = document.querySelector('.component-lesson-left-column');
@@ -9,6 +10,29 @@ if (!commentTextArea) {
   column.appendChild(div);
 }
 
+// for comment
 var script = document.createElement('script');
 script.innerText = 'var commentTextArea = document.querySelector("#comment-text-area");window.addEventListener("appearComment", function(e) {  var comments = window.kyuuri.comments;  var currentComment = comments.find(function(co) { return co.no === e.detail.item.no; });  commentTextArea.value += "\\n" + currentComment.no + ": " + currentComment.text;  commentTextArea.scrollTop = commentTextArea.scrollHeight;}, false);';
 document.body.appendChild(script);
+
+// for lesson interaction comment
+var commentText = null;
+function updateCommentIfChange(commentDiv) {
+  if (commentText !== commentDiv.innerHTML) {
+    commentDiv.addEventListener('DOMNodeRemoved', handleMutations) ;
+    commentText = commentDiv.innerHTML;
+    commentTextArea.value += '\n運営: ' + commentText;
+    commentTextArea.scrollTop = commentTextArea.scrollHeight;
+  }
+}
+function handleMutations(mutations) {
+  var commentDiv = document.querySelector('.component-lesson-interaction-bar-event-message-inner');
+  if (commentDiv) {
+    updateCommentIfChange(commentDiv);
+  }
+}
+var observer = new MutationObserver(handleMutations);
+var config = { attributes: true, childList: true, characterData: true };
+observer.observe(document.querySelector('.component-lesson-interaction-bar'), config);
+
+
