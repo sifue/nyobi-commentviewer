707535f71d0cff67e837805d1882fa6c71de7274
yos10 <61726023+yos10@users.noreply.github.com>
2021-04-23 22:27:09 +0900

    N予備校のサイト変更に対応

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 // create comment area
-var commentTextArea = document.querySelector('#comment-text-area');
+let commentTextArea = document.querySelector('#comment-text-area');
 if (!commentTextArea) {
-  var column = document.querySelector('#root > div > div > div > div > div');
-  var div = document.createElement('div');
+  const column = document.querySelector('#root > div > div > div > div > div');
+  const div = document.createElement('div');
   commentTextArea = document.createElement('textarea');
   commentTextArea.style = 'width:100%;height:6em;';
   commentTextArea.id = 'comment-text-area';
@@ -12,30 +12,41 @@ if (!commentTextArea) {
 }
 
 // for comment (PIXI.js hack)
-var script = document.createElement('script');
-script.innerText = 'var commentTextArea = document.querySelector("#comment-text-area"); var oldText = null; var _drawLetterSpacing = PIXI.Text.prototype.drawLetterSpacing; PIXI.Text.prototype.drawLetterSpacing = function (text, x, y, isStroke) { _drawLetterSpacing.call(this, text, x, y, isStroke); if (oldText != text) { commentTextArea.value += "\\n" + text; oldText = text; commentTextArea.scrollTop = commentTextArea.scrollHeight; } };';
+const script = document.createElement('script');
+script.innerText =
+  'const commentTextArea = document.querySelector("#comment-text-area"); let oldText = null; const _drawLetterSpacing = PIXI.Text.prototype.drawLetterSpacing; PIXI.Text.prototype.drawLetterSpacing = function (text, x, y, isStroke) { _drawLetterSpacing.call(this, text, x, y, isStroke); if (oldText != text) { commentTextArea.value += "\\n" + text; oldText = text; commentTextArea.scrollTop = commentTextArea.scrollHeight; } };';
 document.body.appendChild(script);
 
-
 // for lesson interaction comment
-var commentText = null;
+let commentText = null;
 function updateCommentIfChange(commentDiv) {
   if (commentText !== commentDiv.innerText) {
-    commentDiv.addEventListener('DOMNodeRemoved', handleMutations) ;
     commentText = commentDiv.innerText;
-    if(commentText) {
+    if (commentText) {
       commentTextArea.value += '\n運営: ' + commentText;
       commentTextArea.scrollTop = commentTextArea.scrollHeight;
     }
   }
 }
-function handleMutations(mutations) {
-  var commentDiv = document.querySelector('#root > div > div > div > div > div > div > div > div > div > div');
-  if (commentDiv) {
-    updateCommentIfChange(commentDiv);
-  }
-}
-var observer = new MutationObserver(handleMutations);
-var config = { attributes: true, childList: true, characterData: true, subtree: true };
-observer.observe(document.querySelector('#root > div > div > div > div > div > div > div'), config);
 
+// 動画上部の運営コメントが表示されている部分
+const targetNode = document.querySelector(
+  '#root > div > div > div > div > div > div:nth-child(1) > div > div:nth-child(1)'
+);
+const config = {
+  attributes: true,
+  childList: true,
+  characterData: true,
+  subtree: true,
+};
+
+const callback = (mutationsList, observe) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      updateCommentIfChange(targetNode);
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
