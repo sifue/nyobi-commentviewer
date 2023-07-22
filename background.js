db13adf76658723f2a5828bef16939f86ccd5994
Kaisei Yoneyama <kaisei_yoneyama@dwango.co.jp>
2023-07-22 19:56:29 +0900

    Manifest V3 対応

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 'use strict';
-chrome.webNavigation.onCompleted.addListener(function(data) {
-  if(data.url.includes('nnn.ed.nico/contents/lessons/')) {
-    chrome.tabs.executeScript(null, { file: 'script.js' });
+chrome.webNavigation.onCompleted.addListener(async (details) => {
+  if(details.url.includes('nnn.ed.nico/contents/lessons/')) {
+    await chrome.scripting.executeScript({
+      target: { tabId: details.tabId },
+      files: [ 'script.js' ]
+    });
   }
 });
