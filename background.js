85f3bac325e8c834178eb96cd1dd9b0d8b55a7c4
Kaisei Yoneyama <kaisei_yoneyama@dwango.co.jp>
2023-07-22 20:12:23 +0900

    授業ページのパスを更新

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 'use strict';
 chrome.webNavigation.onCompleted.addListener(async (details) => {
-  if(details.url.includes('nnn.ed.nico/contents/lessons/')) {
+  if(details.url.includes('nnn.ed.nico/lessons/')) {
     await chrome.scripting.executeScript({
       target: { tabId: details.tabId },
       files: [ 'script.js' ]
