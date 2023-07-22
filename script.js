89bd01bfd2d7d87e7951b301b3fd21d9bd0b9758
Kaisei Yoneyama <kaisei_yoneyama@dwango.co.jp>
2023-07-22 22:31:01 +0900

    3 秒後に CSS と JS を追加する

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const link = document.createElement('link');
-link.setAttribute('href', chrome.runtime.getURL('comment-viewer.css'));
-link.setAttribute('rel', 'stylesheet');
-link.setAttribute('type', 'text/css');
-document.head.appendChild(link);
+setTimeout(() => {
+  const link = document.createElement('link');
+  link.setAttribute('href', chrome.runtime.getURL('comment-viewer.css'));
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('type', 'text/css');
+  document.head.appendChild(link);
 
-const script = document.createElement('script');
-script.setAttribute('src', chrome.runtime.getURL('comment-viewer.js'));
-script.setAttribute('type', 'text/javascript');
-document.body.appendChild(script);
+  const script = document.createElement('script');
+  script.setAttribute('src', chrome.runtime.getURL('comment-viewer.js'));
+  script.setAttribute('type', 'text/javascript');
+  document.body.appendChild(script);
+}, 3000);
