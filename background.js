5a36ddf60cea719b7e5c4e1c38f5c30e89f32bbc
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-11-08 18:11:31 +0900

    /courses/:course_id/chapters/:chapter_id/lessons/:lesson_id のページに対応

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
 'use strict';
+
+/**
+ * 授業ページの URL の正規表現
+ * - https://www.nnn.ed.nico/lessons/:lesson_id
+ * - https://www.nnn.ed.nico/courses/:course_id/chapters/:chapter_id/lessons/:lesson_id
+ */
+const urlRegExp = /^https:\/\/www\.nnn\.ed\.nico(?:\/courses\/\d+\/chapters\/\d+)?\/lessons\/\d+$/;
+
 chrome.webNavigation.onCompleted.addListener(async (details) => {
-  if(details.url.includes('nnn.ed.nico/lessons/')) {
+  if (urlRegExp.test(details.url)) {
     await chrome.scripting.executeScript({
       target: { tabId: details.tabId },
       files: [ 'script.js' ]
